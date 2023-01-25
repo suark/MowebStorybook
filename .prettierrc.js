1bdeff3d16509c3c6a20bd8f083a43d8ed6dad64
suark <suark.nairb@gmail.com>
2023-01-25 00:38:36 -0700

    First commit. Ran template then made some minor changes to configs

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  tabWidth: 2,
+  printWidth: 120,
+  semi: true,
+  jsxSingleQuote: true,
+  singleAttributePerLine: true,
+};
